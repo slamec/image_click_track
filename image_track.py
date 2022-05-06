31aba40c5c5989ad68c799df84284d745e916a58
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-06 20:37:13 +0200

    Opens multiple images

--- image_track.py
+++ image_track.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python3
 import cv2 
 import os 
+import matplotlib.pyplot as plt
 
 img_path = input('Imsert image(s) path: ')
 
@@ -9,3 +10,13 @@ def open_img():
     for images in os.listdir(img_path):
         input_path = os.path.join(img_path, images)
 
+
+        image = cv2.imread(str(input_path))
+        image =cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
+        plt.imshow(image)
+        show = plt.show()
+        
+    return show
+
+open_img()

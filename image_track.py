381bd4c1b39250c8a2c09319ce0b71a2d96fd68f
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-06 19:54:27 +0200

    opens files

--- image_track.py
+++ image_track.py
@@ -1,1 +1,11 @@
+#/usr/bin/env python3
 import cv2 
+import os 
+
+img_path = input('Imsert image(s) path: ')
+
+def open_img():
+
+    for images in os.listdir(img_path):
+        input_path = os.path.join(img_path, images)
+

773a10cb1aefd7c09e00b6332f6a0241f04da8ec
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-07 19:35:35 +0200

    creates list of coordinates and file name

--- image_track.py
+++ image_track.py
@@ -5,9 +5,17 @@ import matplotlib.pyplot as plt
 
 img_path = input('Imsert image(s) path: ')
 
+coordinates = []
+
 def mouse_track(event, x, y, flags, param):
+
     if event == cv2.EVENT_LBUTTONDOWN:
-        print (x, y, )
+
+        # to check if coordinates are correct for a file name
+        print(x, y)
+        
+        coordinates.append(x)
+        coordinates.append(y)
 
 def open_img():
 
@@ -19,9 +27,11 @@ def open_img():
         # read and show images - waitKey to keep window open  
         image = cv2.imread(str(input_path))
         
-        cv2.imshow('Preview', image) 
-        cv2.setMouseCallback('Preview', mouse_track) 
+        cv2.imshow(images, image) 
+        cv2.setMouseCallback(images, mouse_track) 
+        coordinates.append(images + ": ")
         cv2.waitKey()
 
-
 open_img()
+
+print(coordinates)

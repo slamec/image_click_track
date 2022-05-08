5a49bb62b6be27496b4981f6188a772adf6f2e3a
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-05-08 20:34:43 +0200

    final version

--- image_track.py
+++ image_track.py
@@ -1,46 +1,66 @@
 #/usr/bin/env python3
+
 import cv2 
 import os 
-import matplotlib.pyplot as plt
 import csv
 
 img_path = input('Imsert image(s) path: ')
 
-coordinates = []
+# coordinates x and y two lists - list_x[0] <-> list_y[0]
+coordinates_x = []
+coordinates_y = []
 
+# key file name, value 2 lists 
+coordinates_dic = {}
 
 def mouse_track(event, x, y, flags, param):
+    """Save mouse left button action to a list. Returns coordinates. Accepts only 5 parameters."""
 
     if event == cv2.EVENT_LBUTTONDOWN:
 
-        # to check if coordinates are correct for a file name
+        # to check if coordinates macth with the filename 
         print(x, y)
-        
-        coordinates.append(x)
-        coordinates.append(y)
+
+        # append list with x and y coordinates
+        coordinates_x.append(x)
+        coordinates_y.append(y)
+
 
 def open_img():
+    """Loops over files in a directory, read image(s), shows, tracks mouse call backs and creates dictionary key = file name, value = lists of x and y coordinates"""
 
-    # loop over images in given directory
+    # loop over image(s) in given directory
     for images in os.listdir(img_path):
-        # directory + files
+
+        # path + files
         input_path = os.path.join(img_path, images)
 
         # read and show images - waitKey to keep window open  
         image = cv2.imread(str(input_path))
         
         cv2.imshow(images, image) 
-        cv2.setMouseCallback(images, mouse_track) 
-        coordinates.append(images + ':')
+        cv2.setMouseCallback(images, mouse_track)
+
+        coordinates_dic[images] = coordinates_x, coordinates_y
+
         cv2.waitKey()
 
+# call the function
 open_img()
 
-print(coordinates)
+print(coordinates_dic)
 
-with open('Coordinates.csv', 'w') as file:
-        write = csv.writer(file)
-        write.writerow(coordinates)
+# save dictionary to a csv file, avoid newline \n
+with open('Coordinates.csv', 'w', newline = '') as file:
+
+    # creating a csv writer object
+    writerfile = csv.writer(file)
+
+    # writing dictionary keys as headings of csv (filename)
+    writerfile.writerow(coordinates_dic.keys())
+
+    # writing list of dictionary
+    writerfile.writerows(zip(*coordinates_dic.values()))
 
 
 
